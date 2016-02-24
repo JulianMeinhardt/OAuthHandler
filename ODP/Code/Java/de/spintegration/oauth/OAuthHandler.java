4fcceb310063f4cc0b39d388ebe3d76373cf6fd1
Julian Meinhardt <julian.meinhardt@sp-integration.de>
2016-02-24 13:49:28 +0100

    added httpclient and httpcore, access token is now generated and received by the handler. Need to store it and use it wisely

--- ODP/Code/Java/de/spintegration/oauth/OAuthHandler.java
+++ ODP/Code/Java/de/spintegration/oauth/OAuthHandler.java
@@ -1,6 +1,18 @@
 package de.spintegration.oauth;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 /**
  * 
@@ -38,7 +50,7 @@ public class OAuthHandler implements Serializable{
 			builder.append(this.appID);
 			builder.append("&");
 			builder.append("callback_uri=");
-			builder.append(thiscallbackURL);
+			builder.append(this.callbackURL);
 			
 			return builder.toString();
 		}else{
@@ -66,6 +78,42 @@ public class OAuthHandler implements Serializable{
 
 		builder.append("grant_type=authorization_code");
 		return builder.toString();
+	}
+	
+	public void refreshShortToken(String code){
+		try{
+			// Build HTTP POST to get full token
+			HttpClient client = new DefaultHttpClient();
+			HttpPost post = new HttpPost("https://apps.na.collabserv.com/manage/oauth2/token");
+			post.addHeader("Content-Type", "application/x-www-form-urlencoded");
+			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+			// Build POST body with all those important things
+			nameValuePairs.add(new BasicNameValuePair("callback_uri", this.callbackURL));
+			nameValuePairs.add(new BasicNameValuePair("client_secret", this.clientSecret));
+			nameValuePairs.add(new BasicNameValuePair("client_id", this.appID));
+			nameValuePairs.add(new BasicNameValuePair("grant_type", "authorization_code"));
+			nameValuePairs.add(new BasicNameValuePair("code", code));
+			// append it to the POST
+			post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+			System.out.println("Exec post with code: " + code);
+			// execute and get response
+			HttpResponse response = client.execute(post);
+			
+			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+			System.out.println("Status code: " + response.getStatusLine().getStatusCode());
+			System.out.println("Content-Type: " + response.getEntity().getContentType());
+		    String line = "";
+		    	while ((line = rd.readLine()) != null) {
+		    		System.out.println(line);
+					if (line.startsWith("access_token=")) {
+						String key = line.substring(12);
+						// do something with the key
+					}
+				}
+		}catch(Exception e){
+			System.out.println("Error while refreshing short token");
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -108,6 +156,20 @@ public class OAuthHandler implements Serializable{
 	 */
 	public void setCallbackURL(String callbackURL) {
 		this.callbackURL = callbackURL;
+	}
+
+	/**
+	 * @return the clientSecret
+	 */
+	public String getClientSecret() {
+		return clientSecret;
+	}
+
+	/**
+	 * @param clientSecret the clientSecret to set
+	 */
+	public void setClientSecret(String clientSecret) {
+		this.clientSecret = clientSecret;
 	}
 }
 
